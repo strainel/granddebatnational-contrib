67e44fa32a51249fb92aeac589724b1ec0c25355
Stéphane Trainel <stephane.trainel@gmail.com>
2019-02-21 18:00:20 +0100

    init

---
+++ create_tmq.R
@@ -0,0 +1,139 @@
+
+setwd("C:/Users/stephane.trainel/Documents/Developpements/granddebat-contrib/")
+
+# Contruction du corpus de mots
+require(stopwords)
+require(tidytext)
+require(tidyverse)
+require(corrplot)
+require(qdapTools)
+
+myTM <- function (d, v, sparsetx = 0.98, maxword = 40, qtx= .8) {
+  corpus <- d %>%
+    mutate(txt = stringr::str_replace_all(v, "’", " ")) %>% 
+    mutate(txt = stringr::str_replace_all(txt, "'", " ")) %>% 
+    mutate(txt = stringr::str_replace_all(txt, '"', " ")) %>% 
+    mutate(txt = stringr::str_replace_all(txt, '“', " ")) %>%
+    mutate(txt = stringr::str_replace_all(txt, '´', " ")) %>%
+    mutate(txt = stringr::str_replace_all(txt, '«', " ")) %>%
+    mutate(txt = stringr::str_replace_all(txt, '»', " ")) %>%
+    .$txt %>% 
+    VectorSource()%>%
+    VCorpus()
+  corpus <- corpus %>%
+    tm_map(content_transformer(tolower))%>%
+    tm_map(stripWhitespace) %>%
+    tm_map(removeNumbers)%>%
+    tm_map(removePunctuation)%>%
+    tm_map(removeWords, c("ainsi","grand","grande","grands","etre","surtous","déjà","ceux","ans","mode","quand","huit","faire","fais","manière","notamment","nombre","car","alors","donc","dont","etc","afin","aussi","comme","chaque","prendre","mettre","être","place","vers","avoir","peut","faut","faudrait","fait","falloir","doivent","doit","autre","autres","niveau")) %>%
+    tm_map(removeWords, stopwords("french")) %>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "ecologie", replacement = "écologie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "écologique", replacement = "écologie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "ecologique", replacement = "écologie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "écologiques", replacement = "écologie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "politiques", replacement = "politique")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "kerosene", replacement = "kérosène")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "kerozene", replacement = "kérosène")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "croisières", replacement = "croisière")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "énergétique", replacement = "énergie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "énergies", replacement = "énergie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "energies", replacement = "énergie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "energie", replacement = "énergie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "développement", replacement = "développer")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "transports", replacement = "transport")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "toutes", replacement = "tous")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "aides", replacement = "aide")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "tva", replacement = "taxe")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "taxer", replacement = "taxe")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "taxes", replacement = "taxe")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "produits", replacement = "produit")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "production", replacement = "produire")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "toutes", replacement = "tous")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "toute", replacement = "tous")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "tout", replacement = "tous")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "solutions", replacement = "solution")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "énergies", replacement = "énergie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "villes", replacement = "ville")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "modes", replacement = "mode")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "véhicules", replacement = "véhicule")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "voitures", replacement = "véhicule")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "voiture", replacement = "véhicule")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "polluantes", replacement = "pollution")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "polluants", replacement = "pollution")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "polluent", replacement = "pollueurs")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "électriques", replacement = "électrique")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "gens", replacement = "particuliers")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "personnes", replacement = "particuliers")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "industriels", replacement = "industrie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "industries", replacement = "industrie")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "aujourd", replacement = "aujourd'hui")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "trie", replacement = "tri")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "utiliser", replacement = "utilisation")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "utilise", replacement = "utilisation")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "locaux", replacement = "local")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "achète", replacement = "acheter")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "achats", replacement = "acheter")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "achat", replacement = "acheter")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "aides", replacement = "aide")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "collectivités", replacement = "collectivité")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "communess", replacement = "commune")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "communes", replacement = "commune")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "communautés", replacement = "communauté")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "entreprises", replacement = "entreprise")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "etat", replacement = "état")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "mairie", replacement = "commune")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "mairies", replacement = "commune")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "départements", replacement = "département")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "régions", replacement = "région")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "comportements", replacement = "comportement")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "solaires", replacement = "solaire")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "impôts", replacement = "impôt")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "importants", replacement = "important")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "climatiques", replacement = "climat")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "climatique", replacement = "climat")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "liés", replacement = "lié")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "européenne", replacement = "européen")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "européens", replacement = "européen")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "financières", replacement = "financière")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "dérèglements", replacement = "dérèglement")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "problemes", replacement = "problème")))%>%
+    tm_map(content_transformer(function(x) gsub(x, pattern = "problèmes", replacement = "problème")))
+  tdm <- corpus %>%
+    DocumentTermMatrix() %>%
+    removeSparseTerms(sparsetx) %>%
+    as.matrix()
+  tdm[tdm>1] <- 1
+  tm <- t(tdm) %*% tdm  
+  
+  maxw <- min(maxword,nrow(tm))
+  rang <- which(diag(tm)<sort(diag(tm), decreasing=TRUE)[maxw])
+  if (length(rang) >0) tm <- tm[-rang,-rang]
+  diag <- diag(tm)
+  diag(tm) <- 0
+  tm[tm<quantile(tm, qtx)] <- 0
+  diag(tm) <- diag
+  
+  rang <- which(colSums(tm)-diag(tm)==0)
+  if (length(rang) >0) tm <- tm[-rang,-rang]
+  tm
+}
+
+#tmq_test <- myTM(data[1:1000,], data[1:1000,]$Q1)
+
+tmq1 <- myTM(data, data$Q1, sparsetx = 0.996)
+tmq2 <- myTM(data, data$Q2)
+tmq4 <- myTM(data, data$Q4, sparsetx = 0.996)
+tmq6 <- myTM(data, data$Q6, maxword = 25, qtx = .6)
+tmq7 <- myTM(data, data$Q7)
+tmq8 <- myTM(data, data$Q8, sparsetx = 0.996)
+tmq10 <- myTM(data, data$Q10, sparsetx = 0.996)
+tmq12 <- myTM(data, data$Q12, sparsetx = 0.996)
+tmq14 <- myTM(data, data$Q14, sparsetx = 0.996)
+tmq15 <- myTM(data, data$Q15)
+tmq16 <- myTM(data, data$Q16)
+
+
+save(vars2_value, data, tmq1,tmq2,tmq4,tmq6,tmq7,tmq8,tmq10,tmq12,tmq14,tmq15,tmq16, file="data2.Rdata")
+# load(file = "data2.Rdata")
+
+
---
+++ loaddata.R
@@ -0,0 +1,65 @@
+
+setwd("C:/Users/stephane.trainel/Documents/Developpements/granddebat-contrib/")
+
+list_module <- c("rjson","httr", "jsonlite", "dplyr", "data.table","tm")
+options(warn = -1)
+require(rjson, quietly = T)
+require(httr, quietly = T)
+require(jsonlite, quietly = T)
+require(dplyr, quietly = T)
+require(data.table, quietly = T) 
+require(tm, quietly = T)
+require(arules)
+
+# Lecture du fichier
+file <- "data/LA_TRANSITION_ECOLOGIQUE.csv"
+pre <- fread(file, sep = ",", header= TRUE, encoding = 'UTF-8')
+
+# Renommage des colonnes
+vars <- c("reference","title","createdAt","publishedAt","updatedAt","trashed","trashedStatus","authorId","authorType","authorZipCode")
+vars2_value <- setdiff(names(pre),vars)
+vars2 <- paste('Q', 1:16, sep='')
+names(pre) <- c(vars, vars2)
+
+# Mise au format date
+pre$createdAt <- as.POSIXct(pre$createdAt, format="%Y-%m-%d %H:%M:%S")
+pre$publishedAt <- as.POSIXct(pre$publishedAt, format="%Y-%m-%d %H:%M:%S")
+pre$updatedAt <- as.POSIXct(pre$updatedAt, format="%Y-%m-%d %H:%M:%S")
+
+pre$authorZipCode[pre$authorZipCode >= 99000] <- NA
+pre$authorZipCode[pre$authorZipCode <= 0] <- NA
+pre$authorZipCode <- sprintf("%05d",as.numeric(pre$authorZipCode))
+pre$dep <- substr(pre$authorZipCode, 0, 2)
+
+pre <- pre %>%
+  mutate(Q1F = case_when(Q1 == "La pollution de l'air"~ Q1,
+                         Q1 == "La biodiversité et la disparition de certaines espèces" ~ Q1,
+                         Q1 == "L'érosion du littoral" ~ Q1,
+                         Q1 == "Les dérèglements climatiques (crue, sécheresse)" ~ Q1,
+                         TRUE ~ "Autre")) %>%
+  mutate(Q1 = case_when(Q1F == "La pollution de l'air"~ "",
+                        Q1F == "La biodiversité et la disparition de certaines espèces" ~ "",
+                        Q1F == "L'érosion du littoral" ~ "",
+                        Q1F == "Les dérèglements climatiques (crue, sécheresse)" ~ "",
+                         TRUE ~ Q1)) %>%
+  mutate(Q1F = case_when(nchar(Q1)==0 & Q1F =="Autre" ~ "", TRUE ~ Q1F)) %>%
+  mutate(Q13_TC = case_when(grepl("Les transports en commun", Q13) ~ "Oui",
+                            TRUE ~ "Non")) %>%
+  mutate(Q13_CO = case_when(grepl("Le covoiturage", Q13) ~ "Oui",
+                            TRUE ~ "Non")) %>%
+  mutate(Q13_AU = case_when(grepl("L'auto partage", Q13) ~ "Oui",
+                            TRUE ~ "Non")) %>%
+  mutate(Q13_OD = case_when(grepl("Le transport à la demande", Q13) ~ "Oui",
+                            TRUE ~ "Non")) %>%
+  mutate(Q13_VE = case_when(grepl("Le vélo", Q13) ~ "Oui",
+                            TRUE ~ "Non")) %>%
+  mutate(Q13_OT = case_when(Q13 == "" ~ "Non", 
+                            !grepl("Le vélo|Le transport à la demande|L'auto partage|Le covoiturage|Les transports en commun", Q13) ~ "Oui",
+                            TRUE ~ "Non")) 
+
+
+
+data <- pre
+
+source("create_tmq.R", encoding = "UTF-8")
+
